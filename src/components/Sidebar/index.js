953151aaa3305a147722c4475702d458c786a275
paul1n4 <paulinevaleroc@gmail.com>
2025-02-28 13:59:56 -0500

    Add isActive and handleScroll for active anchors

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,37 @@
-import { Link } from 'react-router-dom'
 import './index.scss'
 import LogoPV from '../../assets/images/logo-pv.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase, faEnvelope, faGear, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useState, useEffect } from 'react'
 
 const Sidebar = () => {
+  const [isActive, setIsActive] = useState('about')
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      let currentSection = 'home';
+
+      sections.forEach( section => {
+        const sectionTop = section.getBoundingClientRect().top;
+            
+        if (sectionTop <= window.innerHeight / 3) {
+          currentSection = section.id;
+        }
+      });
+
+      setIsActive(currentSection);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className='sidebar'>
       <a className='logo' href='#home'>
@@ -12,22 +39,39 @@ const Sidebar = () => {
       </a> 
 
       <nav>
-        <a href="#home">
+        <a 
+          href="#home"
+          className={`${isActive === 'home' ? 'active' : ''}`}
+          onClick={() => setIsActive('home')}
+        >
           <FontAwesomeIcon icon={faHome} />
         </a>
         <a 
-          className="about-link" 
           href="#about"
+          className={`about-link ${isActive === 'about' ? 'active' : ''}`}
+          onClick={() => setIsActive('about')}
         >
           <FontAwesomeIcon icon={faUser} />
         </a>
-        <a className="skills-link" href="#skills">
+        <a 
+          href="#skills"
+          className={`skills-link ${isActive === 'skills' ? 'active' : ''}`}
+          onClick={() => setIsActive('skills')}
+        >
           <FontAwesomeIcon icon={faGear} />
         </a>
-        <a className="projects-link" href="#projects">
+        <a 
+          href="#projects"
+          className={`projects-link ${isActive === 'projects' ? 'active' : ''}`}
+          onClick={() => setIsActive('projects')}
+        >
           <FontAwesomeIcon icon={faBriefcase} />
         </a>
-        <a className="contact-link" href="#contact">
+        <a 
+          href="#contact"
+          className={`contact-link ${isActive === 'contact' ? 'active' : ''}`}
+          onClick={() => setIsActive('contact')}
+        >
           <FontAwesomeIcon icon={faEnvelope} />
         </a>
       </nav>   
